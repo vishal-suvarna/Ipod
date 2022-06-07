2fb102518ff7fb1d7c68b4e46565d5a2a768d373
Vishal Suvarna <vishalsuvarna2000@gmail.com>
2022-06-07 09:10:04 +0530

    Wheel UI designed

--- src/Ipod.js
+++ src/Ipod.js
@@ -1,12 +1,44 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-
-function Ipod(props) {
-  return (
-    <div>Ipod</div>
-  )
+import React, { Component } from "react";
+import "./index.css";
+class Ipod extends Component {
+  render() {
+    return (
+      <div id="ipodContainer">
+        <div id="btnsContainer">
+          <div id="wheel">
+          <div id="btns">
+              <span id="menuBtn" className="actionBtn">
+                MENU
+              </span>
+              <span id="forwardBtn" className="actionBtn">
+                <img src="https://cdn-icons-png.flaticon.com/512/26/26309.png"></img>
+              </span>
+              <span id="playPauseBtn" className="actionBtn">
+                <img src="https://cdn-icons-png.flaticon.com/512/7109/7109152.png" />
+              </span>
+              <span id="backwardBtn" className="actionBtn">
+                <img src="https://cdn-icons-png.flaticon.com/512/4029/4029036.png"></img>
+              </span>
+            </div>
+            <div id="innerWheel"></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-Ipod.propTypes = {}
-
-export default Ipod
+const styles = {
+  ipodContainer: {
+    width: "100px",
+    backgroundColor: "red",
+    diplay: "flex",
+  },
+  wheel: {
+    height: "50px",
+    width: "50px",
+    borderRadius: "50px",
+    backgroundColor: "grey",
+  },
+};
+export default Ipod;
